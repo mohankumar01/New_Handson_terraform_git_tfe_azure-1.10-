ff40f78d4b71c12817009efab43617b07a17852e
MK <mohanpragath@gmail.com>
2025-10-01 12:31:59 +0530

    n

--- subnet.tf
+++ subnet.tf
@@ -8,5 +8,5 @@ resource "azurerm_subnet" "web-sn" {
   name                 = "app-sn-terra-git-demo"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vn.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
